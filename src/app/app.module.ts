134bfee3b5fc56e97edb40d87c78aedf8e7ca8d8
amandeep.saluja <amandeepsaluja25@gmail.com>
2021-03-27 01:09:41 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { InfoComponent } from './info/info.component';
+import { AboutComponent } from './about/about.component';
+import { FocusAreaComponent } from './focus-area/focus-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent,
+    InfoComponent,
+    AboutComponent,
+    FocusAreaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/focus-area/focus-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-focus-area',
+  templateUrl: './focus-area.component.html',
+  styleUrls: ['./focus-area.component.scss']
+})
+export class FocusAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
